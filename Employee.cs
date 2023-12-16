689e2bf081cd3d209726355b3590b06411f26142
NaderMamdouh99 <Nader.mamdouh9898@gmail.com>
2023-12-16 16:06:07 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Day_2
+{
+    public class Employee
+    {
+        #region Create Variables
+        string _Name;
+        string _Adderss;
+        int _NID;
+        int _Salary;
+        public string _Tittle;
+        static string _DepartmentName;
+        static int Count;
+        #endregion
+
+        #region Default Constructor
+        public Employee()
+        {
+            _Name = "N/A";
+            _Adderss = "Minia";
+            _NID = 1;
+            _Salary = 4000;
+            _Tittle = "Iti student";
+            _DepartmentName = "“.Net Department";
+            Count++;
+
+        }
+
+        public Employee(string name, string address, int Nid, int salary, string tittle)
+        {
+            if (name.Length > 2)
+            {
+                _Name = name;
+            }
+            else
+            {
+                _Name = "N/A";
+            }
+            if (address != null)
+            {
+                _Adderss = address;
+            }
+            else
+            {
+                _Adderss = "Minia";
+            }
+            if (Nid != 0)
+            {
+                _NID = Nid;
+            }
+            else
+            {
+                _NID = 1;
+            }
+            if (salary != 0 && salary > 3000)
+            {
+                _Salary = salary;
+            }
+            else
+            {
+                _Salary = 4000;
+            }
+            if (tittle.Length > 2)
+            {
+                _Tittle = tittle;
+            }
+            else
+            {
+                _Tittle = "Iti student";
+            }
+            _DepartmentName = "“.Net Department";
+            Count++;
+
+
+        }
+        public Employee(string name, string address, int Nid, int salary ) : this(name,address,Nid,salary, "Iti student")
+        {
+
+        }
+        #endregion
+
+        #region function get and set
+        public int Sallary
+        {
+            get { return _Salary; }
+            set { _Salary = value; }
+        }
+        #endregion
+
+        #region Static Constructor
+        static Employee()
+        {
+            _DepartmentName=  ".Net Department";
+             Count = 0;
+        }
+        #endregion
+
+        #region Function Display 
+        public void Display()
+        {
+            Console.WriteLine($"Name:{_Name}\n Address : {_Adderss}\n Nid:{_NID}\n Salary:{_Salary}\n Tittle:{_Tittle}");
+        }
+        public static void DisplayCount()
+        {
+            Console.WriteLine(Count);
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+using System.Net;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+namespace OOP_Day_2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Assignment
+			//1-Design class that contain info(Name, Address, NID, Salary, Title, DepartmentName).
+			//2-There is only one department which is “.Net Department”, choose the most appropriate type for this field.
+			//3-Create all type of constructors needed.
+			//4-Use Constructor Chaining.
+			//5-Use Property to define class member if needed.
+			//6-Try to set constrain in your Property in set and get  
+			//7-Add static field “Count” to count number of objects created.
+			//8-Add a member function called “DisplayCount” to display the total number of employees.
+			//9-Call the function DisplayCount in main.
+				//a.Can you call the function using object?
+			//10-	Add a static constructor.
+				//a.Can you call the static constructor?
+				//b.What is the difference between instance constructor and static constructor?
+
+	#endregion
+
+			#region Lab
+			Employee employee = new Employee();
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee("Nader", "Bns", 2, 8000);
+            employee2.Display();
+            Employee.DisplayCount(); 
+            #endregion
+        }
+    }
+}
